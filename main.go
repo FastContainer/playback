4fc1aa91c4d887bc5dae455c830ce400804b770d
linyows <linyows@gmail.com>
2019-07-11 16:00:15 +0900

    update name

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ type BulkMail struct {
 
 var cmder Cmder = Cmd{}
 
+const subject = "fast container"
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
@@ -29,16 +31,16 @@ func main() {
 	}
 
 	totalTime := 10 * time.Second
-	case1 := &BulkMail{from: "root@example-a.local", to: "root@smtp-rcpt", subject: "aaa", sessionCount: 10, messageCount: 100, interval: 2}
-	case2 := &BulkMail{from: "root@example-b.local", to: "root@smtp-rcpt", subject: "bbb", sessionCount: 1, messageCount: 1, interval: 3}
-	case3 := &BulkMail{from: "root@example-c.local", to: "ca@smtp-tarpit", subject: "ccc", sessionCount: 1, messageCount: 1, interval: 5}
-	case4 := &BulkMail{from: "root@example-d.local", to: "root@smtp-rcpt", subject: "ddd", sessionCount: 100, messageCount: 1000, interval: 5}
+	case1 := &BulkMail{from: "root@mail1.test", to: "root@smtp-rcpt", subject: subject, sessionCount: 10, messageCount: 100, interval: 2}
+	case2 := &BulkMail{from: "root@mail2.test", to: "root@smtp-rcpt", subject: subject, sessionCount: 1, messageCount: 1, interval: 3}
+	case3 := &BulkMail{from: "root@mail3.test", to: "root@smtp-tarpit", subject: subject, sessionCount: 1, messageCount: 1, interval: 5}
+	case4 := &BulkMail{from: "root@mail4.test", to: "root@smtp-rcpt", subject: subject, sessionCount: 100, messageCount: 1000, interval: 5}
 
 	// Playback 1: Containers
-	job1, _ := scheduler.Every(case1.interval).Seconds().Run(func() { case1.send("containers:58025") })
-	job2, _ := scheduler.Every(case2.interval).Seconds().Run(func() { case2.send("containers:58026") })
-	job3, _ := scheduler.Every(case3.interval).Seconds().Run(func() { case3.send("containers:58027") })
-	job4, _ := scheduler.Every(case4.interval).Seconds().Run(func() { case4.send("containers:58028") })
+	job1, _ := scheduler.Every(case1.interval).Seconds().Run(func() { case1.send("mail1.test:58025") })
+	job2, _ := scheduler.Every(case2.interval).Seconds().Run(func() { case2.send("mail2.test:58026") })
+	job3, _ := scheduler.Every(case3.interval).Seconds().Run(func() { case3.send("mail3.test:58027") })
+	job4, _ := scheduler.Every(case4.interval).Seconds().Run(func() { case4.send("mail4.test:58028") })
 
 	// Playback 2: Monolithic
 	monolithic := "monolithic:58025"
