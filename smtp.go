c8037802255a688fb7a0c95ea64f1e08906307b2
linyows <linyows@gmail.com>
2019-10-28 18:20:16 +0900

    update smtp.go

--- smtp.go
+++ smtp.go
@@ -1,50 +1,86 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net"
 	"net/textproto"
 )
 
 func main() {
-	run()
-}
+	rcptAddr := "root@recipient"
+	fromAddr := "root@sender"
 
-type Client struct {
-	conn *textproto.Conn
-}
-
-// https://golang.org/src/net/smtp/smtp.go
-// https://golang.org/src/net/textproto/textproto.go
-func run() {
-	client, err := Dial("recipient:25")
+	c, err := Dial("recipient:25")
 	if err != nil {
 		fmt.Printf("Dial\n%#v\n", err)
 		return
 	}
-	err := client.Helo("localhost")
+	err = c.Helo("localhost")
 	if err != nil {
 		return
 	}
-	err := client.Mail("root@sender")
+	err = c.MailFrom(fromAddr)
+	if err != nil {
+		return
+	}
+	err = c.RcptTo(rcptAddr)
+	if err != nil {
+		return
+	}
+	err = c.Data()
 	if err != nil {
 		return
 	}
 
-	text, err := client.conn.ReadDotBytes()
+	body := "To: %s\r\n" +
+		"Subject: %s\r\n" +
+		"\r\n" +
+		"This is the email body.\r\n"
+	code, msg, err := c.Cmd(250, body, rcptAddr, "Hello, Gophers")
 	if err != nil {
-		fmt.Printf("ddd\n%#v\n", err)
+		fmt.Printf("HELO\n%d -- %s\n%#v", code, msg, err)
 		return
 	}
-	fmt.Printf("!!!!!!%#v\n", text)
-
-	client.conn.ReadCodeLine(250)
 	return
 }
 
+type Client struct {
+	conn *Conn
+}
+
+type Conn struct {
+	textproto.Reader
+	textproto.Writer
+	textproto.Pipeline
+	conn io.ReadWriteCloser
+}
+
+func (c *Conn) Close() error {
+	return c.conn.Close()
+}
+
+func (c *Conn) Cmd(format string, args ...interface{}) (id uint, err error) {
+	id = c.Next()
+	c.StartRequest(id)
+	err = c.PrintfLine(format, args...)
+	c.EndRequest(id)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
 func Dial(addr string) (*Client, error) {
-	conn, err := textproto.Dial("tcp", addr)
+	c, err := net.Dial("tcp", addr)
 	if err != nil {
 		return nil, err
+	}
+	conn := &Conn{
+		Reader: textproto.Reader{R: bufio.NewReader(c)},
+		Writer: textproto.Writer{W: bufio.NewWriter(c)},
+		conn:   c,
 	}
 	_, _, err = conn.ReadResponse(220)
 	if err != nil {
@@ -55,7 +91,10 @@ func Dial(addr string) (*Client, error) {
 }
 
 func (c *Client) Cmd(expectCode int, format string, args ...interface{}) (int, string, error) {
-	id, err := c.conn.Cmd(format, args)
+	id := c.conn.Next()
+	c.conn.StartRequest(id)
+	err := c.conn.PrintfLine(format, args...)
+	c.conn.EndRequest(id)
 	if err != nil {
 		return 0, "", err
 	}
@@ -68,17 +107,31 @@ func (c *Client) Cmd(expectCode int, format string, args ...interface{}) (int, s
 func (c *Client) Helo(host string) error {
 	code, msg, err := c.Cmd(250, "HELO %s", host)
 	if err != nil {
-		fmt.Printf("HELO\n%d -- %s\n%#v", code, msg, err)
-		return
+		fmt.Printf("HELO %s\nID: %d -- %s\n%#v\n", host, code, msg, err)
 	}
 	return err
 }
 
-func (c *Client) Mail(from string) error {
-	code, msg, err := c.cmd(250, "MAIL FROM:<%s>", from)
+func (c *Client) MailFrom(addr string) error {
+	code, msg, err := c.Cmd(250, "MAIL FROM: "+addr)
 	if err != nil {
-		fmt.Printf("MAIL FROM\n%d -- %s\n%#v", code, msg, err)
-		return
+		fmt.Printf("MAIL FROM: <%s>\nID: %d -- %s\n%#v\n", addr, code, msg, c.conn)
+	}
+	return err
+}
+
+func (c *Client) RcptTo(addr string) error {
+	code, msg, err := c.Cmd(25, "RCPT TO: <%s>", addr)
+	if err != nil {
+		fmt.Printf("RCPT TO: <%s>\nID: %d -- %s\n%#v\n", addr, code, msg, c.conn)
+	}
+	return err
+}
+
+func (c *Client) Data() error {
+	code, msg, err := c.Cmd(354, "DATA")
+	if err != nil {
+		fmt.Printf("DATA\nID: %d -- %s\n%#v\n", code, msg, c.conn)
 	}
 	return err
 }
